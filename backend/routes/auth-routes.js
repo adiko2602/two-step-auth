0c40655433aeb62bf2a4014227da0e46f93cb29d
B34TER <sew.szymanski@gmail.com>
2023-04-09 15:10:21 +0200

    Add rateLimiter and token verification in auth routes

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,7 +1,28 @@
 const router = require('express').Router()
 const auth = require('../controllers/auth-controller')
+const rateLimit = require("express-rate-limit")
+const verifyToken = require('../middleware/verify-token');
 
-router.post('/login', auth.login)
+const LOGIN_WINDOW_MS = 5 * 60 * 1000;
+const LOGIN_REQUESTS_LIMIT = 10;
+const TOTP_WINDOW_MS = 3 * 60 * 1000;
+const TOTP_REQUESTS_LIMIT = 10;
+
 router.post('/register', auth.register)
+router.post('/login',
+    rateLimit({
+        windowMs: LOGIN_WINDOW_MS,
+        max: LOGIN_REQUESTS_LIMIT,
+    }),
+    auth.login
+)
+router.post('/verify-totp',
+    verifyToken,
+    rateLimit({
+        windowMs: TOTP_WINDOW_MS,
+        max: TOTP_REQUESTS_LIMIT,
+    }),
+    auth.verifyTOTP
+)
 
 module.exports = router
