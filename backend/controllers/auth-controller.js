cbf6de580f62e7fecf86697961fbc08059e8c765
B34TER <sew.szymanski@gmail.com>
2023-04-09 15:10:31 +0200

    Update auth controller

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,34 +1,39 @@
+const generateUserSecret = require('../utils/generate-user-secret');
 const sendResponse = require('../utils/send-response')
+const generateTOTP = require('../../services/totp/generator')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const User = require('../models/user-model')
 
 const SALT_ROUNDS = 10;
+const JWT_EXPIRiATION_TIME = '5d'
+const TEMP_JWT_EXPIRiATION_TIME = '3m'
 
 const login = async (req, res) => {
     const {
-        email, 
+        email,
         password
     } = req.body
 
     const user = await User.findOne({ email: email })
-    
+
     if (!user || !await bcrypt.compare(password, user?.password)) {
         return res.status(400).json(sendResponse(false, 'Invalid credentials'))
     }
 
-    const loginToken = jwt.sign(
-        { 'email': user.email },
-        process.env.LOGIN_TOKEN_SECRET, 
-        { expiresIn: 5 * 60 }
+    const tempJWT = jwt.sign(
+        { userId: user.id },
+        process.env.LOGIN_TOKEN_SECRET,
+        { expiresIn: TEMP_JWT_EXPIRiATION_TIME }
     );
-
-    res.status(200).json(sendResponse(true, 'User logged in', loginToken))
+    
+    res.status(200).json(sendResponse(true, 'User logged in', { tempJWT: tempJWT }))
 }
 
 const register = async (req, res) => {
     const {
-        email, 
+        email,
         password
     } = req.body
 
@@ -39,17 +44,41 @@ const register = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, await bcrypt.genSalt(SALT_ROUNDS))
 
+    const secret = generateUserSecret()
     const createdUser = new User({
         email: email,
         password: hashedPassword,
+        secret: secret,
     })
 
-    await createdUser.save();
-    
-    res.status(200).json(sendResponse(true, `Created new account`))
+    await createdUser.save()
+
+    res.status(200).json(sendResponse(true, `Created new account`, { userSecret: secret }))
+}
+
+const verifyTOTP = async (req, res) => {
+    const { TOTP } = req.body;
+
+    const user = await User.findById(req.userId);
+    const sharedSecret = user.secret;
+
+    const serverTOTP = generateTOTP(sharedSecret);
+
+    if (TOTP === serverTOTP) {
+        const JWT = jwt.sign(
+            { userId: user.id },
+            process.env.LOGIN_TOKEN_SECRET,
+            { expiresIn: JWT_EXPIRiATION_TIME }
+        );
+
+        res.status(200).json(sendResponse(true, 'TOTP verification successful', { JWT: JWT }));
+    } else {
+        res.status(400).json(sendResponse(false, 'Invalid TOTP token'));
+    }
 }
 
 module.exports = {
     login,
     register,
+    verifyTOTP,
 }
